dcfd29ed18a9571681b3de7f64a9e50b2e1f6df2
Jeffreyjacob <jeffreyigiemeh16@gmail.com>
2024-03-24 17:33:47 -0700

    new commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,10 +4,13 @@ import { Link, Stack } from 'expo-router'
 import ExploreHeader from '@/components/ExploreHeader'
 import Listing from '@/components/Listing';
 import ListingData from '@/assets/data/airbnb-listings@public (4).json';
+import ListingsMap from '@/components/ListingsMap';
+import ListingDataGeo from '@/assets/data/airbnb-listings@public.geo.json';
 
 const Explore = () => {
   const [Category,setCategory] = useState('Tiny homes')
-const items = useMemo(()=>ListingData as any,[])
+const items = useMemo(()=>ListingData as any,[]);
+const geo = useMemo(()=>ListingDataGeo as any,[]);  
 
   const onDataChnaged = (category:string)=>{
      setCategory(category);
@@ -18,7 +21,9 @@ const items = useMemo(()=>ListingData as any,[])
       <Stack.Screen options={{
         header:()=><ExploreHeader onCategoryChanged={onDataChnaged}/>
       }}/>
-      <Listing listings={items} category={Category}/>
+      {/*<Listing listings={items} category={Category}/>*/}
+
+      <ListingsMap listings={geo}/>
     </View>
   )
 }
--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,12 +1,81 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import  { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { useNavigation, useRouter } from 'expo-router';
+import MapView from 'react-native-map-clustering';
 
-const ListingsMap = () => {
+interface Props{
+  listings:any;
+}
+
+const INITIAL_REGION = {
+  latitude:46.204391,
+  longitude:6.143158,
+  latitudeDelta:3,
+  longitudeDelta:2
+}
+
+const ListingsMap = ({listings}:Props) => {
+  const router = useRouter()
+  const onMarkerSelected = (event:any)=>{
+        router.push(`/Listing/${event.properties.id}`)
+  }
   return (
-    <View>
-      <Text>ListingsMap</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map}
+      animationEnabled={false}
+      provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_REGION}
+        clusterColor='#fff'
+        clusterTextColor='#000'
+      >
+        {listings.features.map((item:any)=>(
+          <Marker 
+          key={item.properties.id}
+          onPress={()=>onMarkerSelected(item)}
+          coordinate={{
+            latitude: +item.properties.latitude,
+            longitude:+item.properties.longitude
+          }}>
+            <View style={styles.marker}> 
+              <Text style={styles.markerText}>
+              € {item.properties.price}
+              </Text>
+            </View>
+          </Marker>
+        ))}
+      </MapView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  },
+  map:{
+    width:'100%',
+    height:'100%'
+  },
+  marker:{
+     backgroundColor:'#fff',
+     borderRadius:12,
+     padding:6,
+     justifyContent:'center',
+     alignItems:'center',
+     elevation:5,
+     shadowColor:'#000',
+     shadowOpacity:0.1,
+     shadowRadius:6,
+     shadowOffset:{
+      width:1,
+      height:10
+     }
+  },
+  markerText:{
+     fontSize:14,
+     fontFamily:'MonSb'
+  }
+})
 
 export default ListingsMap
